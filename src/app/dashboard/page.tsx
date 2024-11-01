39c43b7a60ae74f2780680334d8c28d111a069c0
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-11-02 01:17:31 +0800

    fin CreateEventCategoryModal

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import { db } from "@/db"
 import { currentUser } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
 import { DashboardPageContent } from "./dashboard-page-content"
-// import { CreateEventCategoryModal } from "@/components/create-event-category-modal"
+import { CreateEventCategoryModal } from "@/components/create-event-category-modal"
 import { Button } from "@/components/ui/button"
 import { PlusIcon } from "lucide-react"
 // import { createCheckoutSession } from "@/lib/stripe"
@@ -48,14 +48,14 @@ const Page = async ({ searchParams }: PageProps) => {
       {/* {success ? <PaymentSuccessModal /> : null} */}
 
       <DashboardPage
-        // cta={
-        //   <CreateEventCategoryModal>
-        //     <Button className="w-full sm:w-fit">
-        //       <PlusIcon className="size-4 mr-2" />
-        //       Add Category
-        //     </Button>
-        //   </CreateEventCategoryModal>
-        // }
+        cta={
+          <CreateEventCategoryModal>
+            <Button className="w-full sm:w-fit">
+              <PlusIcon className="size-4 mr-2" />
+              Add Category
+            </Button>
+          </CreateEventCategoryModal>
+        }
         title="Dashboard"
       >
         <DashboardPageContent />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,7 +28,7 @@ const config: Config = {
   				'900': '#1B2A61',
   				'950': '#111A3E'
   			},
-			'discord-background': '#36393f',
+  			'discord-background': '#36393f',
   			'discord-gray': '#36393f',
   			'discord-text': '#dcddde',
   			'discord-timestamp': '#72767d',
