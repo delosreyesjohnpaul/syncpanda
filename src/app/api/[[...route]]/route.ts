a23dd952208e5df3c1ccc7e8ec5fda0907808cd9
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-11-03 19:51:09 +0800

    remove the fckin edge

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,5 +1,6 @@
-import { httpHandler } from "@/server"
+import { httpHandler } from "@/server";
 
-export const runtime = "edge"
+// Remove the edge runtime setting
+// export const runtime = "edge";  // Comment this line out or remove it
 
-export { httpHandler as GET, httpHandler as POST }
+export { httpHandler as GET, httpHandler as POST };
